9781fbce2e6ada416337b3dfb85220c54e81062c
Sumit Garg <sumit.garg@oss.qualcomm.com>
2025-12-24 16:22:19 +0530

    plat-qcom: Add support for lemans platform
    Signed-off-by: Sumit Garg <sumit.garg@oss.qualcomm.com>

--- core/arch/arm/plat-qcom/platform_config.h
+++ core/arch/arm/plat-qcom/platform_config.h
@@ -12,7 +12,7 @@
 /* Make stacks aligned to data cache line length */
 #define STACK_ALIGNMENT			64
 
-#if defined(PLATFORM_FLAVOR_kodiak)
+#if defined(PLATFORM_FLAVOR_kodiak) || defined(PLATFORM_FLAVOR_lemans)
 #define DRAM0_BASE			UL(0x80000000)
 #define DRAM0_SIZE			UL(0x80000000)
 #define DRAM1_BASE			ULL(0x100000000)
@@ -22,13 +22,18 @@
 #define RAMBLUR_PIMEM_VAULT_TA_BASE	ULL(0xc1800000)
 #define RAMBLUR_PIMEM_VAULT_TA_SIZE	ULL(0x01c00000)
 
-#define GENI_UART_REG_BASE		UL(0x994000)
 #define RAMBLUR_PIMEM_REG_BASE		UL(0x610000)
 #define SEC_PRNG_REG_BASE		UL(0x010D1000)
 
 /* GIC related constants */
 #define GICD_BASE			UL(0x17a00000)
 #define GICR_BASE			UL(0x17a60000)
+#endif
+
+#if defined(PLATFORM_FLAVOR_kodiak)
+#define GENI_UART_REG_BASE		UL(0x994000)
+#elif defined(PLATFORM_FLAVOR_lemans)
+#define GENI_UART_REG_BASE		UL(0xa8c000)
 #endif
 
 #endif /*PLATFORM_CONFIG_H*/
